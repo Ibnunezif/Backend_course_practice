37dd77dc30c545190d0f7307b5393b75a8ddd7c9
Abdulbasit Nezif Abasambi <142674760+Ibnunezif@users.noreply.github.com>
2025-02-27 16:19:01 +0300

    family Map travel tracker and premalist projects are completed

--- 016 Permalist-Project/8.6 Permalist Project/index.js
+++ 016 Permalist-Project/8.6 Permalist Project/index.js
@@ -9,10 +9,7 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// let items = [
-//   { id: 1, title: "Buy milk" },
-//   { id: 2, title: "Finish homework" },
-// ];
+
 const db= new pg.Client({
   user:"postgres",
   password:"12345678",
